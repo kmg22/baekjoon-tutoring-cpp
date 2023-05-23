10124da0c98a9d48d99223bf09a9a6a6f94797a9
doneni <xxerrenn@icloud.com>
2023-05-23 17:50:27 +0900

    src updated

--- week07/src/01_2635.cpp
+++ week07/src/01_2635.cpp
@@ -3,9 +3,10 @@ using namespace std;
 
 int main(void)
 {
-    int n = 100;
+    int n = 0;
+    cin >> n;
     int max= 0, second =0;
-    for (int i = 1; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
         int n1 = n, n2 = i, cnt = 2;
         while(1)
@@ -19,10 +20,11 @@ int main(void)
     }
     cout << max << endl;
     int n1 = n, n2 = second;
-    for(int i = 0; i < max; i++)
+    cout << n << " " << second << " ";
+    for(int i = 0; i < max - 2; i++)
     {
-        cout << n1 - n2 << " ";
-        n2 = n - second;
+        cout << n1 -  n2 << " ";
+        n2 = n1 - n2;
         n1 -= n2;
     }
     return 0;
